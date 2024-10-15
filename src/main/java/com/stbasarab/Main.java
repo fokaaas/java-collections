7be91bc493f66a7d8f60d91da5bd79c4b7e75aec
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-15 16:16:16 +0300

    feat(Main): added entry point

--- src/main/java/com/stbasarab/Main.java
+++ src/main/java/com/stbasarab/Main.java
@@ -1,8 +1,56 @@
 package com.stbasarab;
 
+import com.stbasarab.collections.ElectricalApplianceList;
+
+import java.util.Arrays;
+
+/**
+ * Main class to demonstrate the usage of the ElectricalApplianceList collection
+ * with ElectricalAppliance instances.
+ */
 public class Main {
 
+    /**
+     * Main method to demonstrate the usage of the ElectricalApplianceList collection
+     * with ElectricalAppliance instances.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
+        ElectricalApplianceList applianceList = new ElectricalApplianceList();
 
+        applianceList.add(new ElectricalAppliance("Refrigerator", 150, 0.5));
+        applianceList.add(new ElectricalAppliance("Microwave", 800, 1.2));
+        applianceList.add(new ElectricalAppliance("Vacuum Cleaner", 1200, 0.3));
+
+
+        // Displaying the list of appliances
+        System.out.println("Appliances:");
+        printApplianceList(applianceList);
+
+        // Getting a specific appliance
+        ElectricalAppliance appliance = applianceList.get(1);
+        System.out.println("Second appliance: " + appliance.toString());
+
+        // Replacing an appliance
+        ElectricalAppliance oven = new ElectricalAppliance("Oven", 1500, 1.5);
+        applianceList.set(1, oven);
+        System.out.println("After replacement: ");
+        printApplianceList(applianceList);
+
+        // Removing an appliance
+        applianceList.remove(oven);
+        System.out.println("After removal: ");
+        printApplianceList(applianceList);
+
+        // Clearing the list
+        applianceList.clear();
+        System.out.println("After clearing, number of appliances: " + applianceList.size());
+    }
+
+    private static void printApplianceList(ElectricalApplianceList applianceList) {
+        for (ElectricalAppliance appliance : applianceList) {
+            System.out.println(appliance.toString());
+        }
     }
 }
