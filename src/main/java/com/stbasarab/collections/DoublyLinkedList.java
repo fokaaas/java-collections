28c07f6920f8c44ef0d58ec6515ae2afbb9a4193
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-15 16:15:01 +0300

    feat(DoubleLinkedList): implemented class

---
+++ src/main/java/com/stbasarab/collections/DoublyLinkedList.java
@@ -0,0 +1,129 @@
+package com.stbasarab.collections;
+
+public class DoublyLinkedList<T> {
+    private class Node {
+        T data;
+        Node next;
+        Node prev;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void add(T data) {
+        Node newNode = new Node(data);
+        if (size == 0) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    public void add(int index, T data) {
+        if (isNotValidIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node newNode = new Node(data);
+        if (index == 0) {
+            if (size == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        } else if (index == size) {
+            add(data);
+        } else {
+            Node current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            newNode.prev = current.prev;
+            newNode.next = current;
+            current.prev.next = newNode;
+            current.prev = newNode;
+        }
+        size++;
+    }
+
+    public T get(int index) {
+        if (isNotValidIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public T remove(int index) {
+        if (isNotValidIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        } else {
+            head = current.next;
+        }
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        } else {
+            tail = current.prev;
+        }
+        size--;
+        return current.data;
+    }
+
+    private boolean isNotValidIndex(int index) {
+        return index < 0 || index >= size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean contains(Object o) {
+        Node current = head;
+        while (current != null) {
+            if (current.data.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void set(int index, T data) {
+        if (isNotValidIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.data = data;
+    }
+}
